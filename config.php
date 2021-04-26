f76a5b6cd441309cb321a2ffab1c94e526128654
RichardRojas13 <ri.rojas@duocuc.cl>
2021-04-26 15:38:43 -0400

    a

--- config.php
+++ config.php
@@ -1,23 +1,12 @@
 <?php
-
 define('USER', 'root');
-
 define('PASSWORD', '');
-
 define('HOST', 'localhost');
-
 define('DATABASE', 'ejercicio');
 
- 
-
 try {
-
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
-
 } catch (PDOException $e) {
-
     exit("Error: " . $e->getMessage());
-
 }
-
 ?>
